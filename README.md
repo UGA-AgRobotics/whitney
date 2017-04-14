824d0c5fe0da5a8a8a4670718344fbcb4b65c09a
Brad Bazemore <bwbazemore@uga.edu>
2017-04-13 20:58:07 -0400

    added source and test images

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-![whitney](/img/whitney.jpg)  
+![whitney](/whitney.jpg)  
 # whitney
 [![Build Status](https://travis-ci.org/sonyccd/whitney.svg?branch=master)](https://travis-ci.org/sonyccd/whitney)
 
---
+++ whitney/find_cotton.py
@@ -0,0 +1,73 @@
+import matplotlib.pyplot as plt
+from skimage import io, filters, morphology, exposure, color, feature, segmentation, graph, draw
+from skimage.feature import blob_doh
+from scipy import ndimage as ndi
+import numpy as np
+
+
+def test_lines(img):
+    gauss = filters.gaussian(img, sigma=2)
+    equalize = exposure.equalize_adapthist(gauss)
+    # otsu = filters.threshold_otsu(equalize)
+    # thresh = gauss <= otsu
+    # close = morphology.binary_closing(thresh)
+    # opening = morphology.binary_opening(close)
+    edges = feature.canny(equalize, sigma=4)
+    return edges
+
+
+def test_thresh(img):
+    gauss = filters.gaussian(img, sigma=2)
+    equalize = exposure.equalize_adapthist(gauss)
+    fig, ax = filters.try_all_threshold(equalize)
+
+
+image = io.imread("cotton1.png")
+
+image_gray = color.rgb2gray(image)
+image_gray = exposure.equalize_adapthist(image_gray)
+out = filters.rank.tophat(image_gray, morphology.disk(10))
+close = morphology.closing(out)
+opening = morphology.opening(close)
+bilat_img = filters.rank.mean_bilateral(opening, morphology.disk(30), s0=10, s1=10)
+
+blobs_doh = blob_doh(bilat_img, min_sigma=20, max_sigma=50, num_sigma=20, threshold=.006)
+
+fig, ax = plt.subplots(1, 1, sharex=True, sharey=True,
+                       subplot_kw={'adjustable': 'box-forced'})
+
+ax.imshow(image, interpolation='nearest', cmap='gray')
+for blob in blobs_doh:
+    y, x, r = blob
+    c = plt.Circle((x, y), r, color='red', linewidth=2, fill=False)
+    rr, cc = draw.circle(y, x, r, shape=image.shape)
+
+    ax.add_patch(c)
+
+ax.text(1, 1, "Bolls: "+str(len(blobs_doh)), color='white',
+        bbox={'facecolor': 'black', 'alpha': 1, 'pad': 2})
+ax.set_axis_off()
+
+plt.tight_layout()
+plt.show()
+
+# r = image[..., 0]
+# g = image[..., 1]
+# b = image[..., 2]
+# gauss = filters.gaussian(image_gray, sigma=0.5)
+# equalize = exposure.equalize_adapthist(gauss)
+# otsu = filters.threshold_otsu(equalize)
+# thresh = gauss <= otsu
+# thresh = 1 - thresh
+# close = morphology.binary_closing(thresh)
+# opening = morphology.binary_opening(close)
+# plt.imshow(opening, cmap='gray')
+
+# plt.figure(1)
+# plt.imshow(test_process(r))
+# plt.figure(2)
+# plt.imshow(test_process(g))
+# plt.figure(3)
+# plt.imshow(test_process(b))
+
+# plt.show()
