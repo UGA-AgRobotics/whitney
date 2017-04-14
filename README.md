b44cffd01a29661060bee7289f029f0b64350046
Brad Bazemore <bwbazemore@uga.edu>
2017-04-13 21:06:10 -0400

    update codeacy badge

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ![whitney](/whitney.jpg)  
 # whitney
-[![Codacy Badge](https://api.codacy.com/project/badge/Grade/15de6cdaace9408e982ad2db14930c1a)](https://www.codacy.com/app/snakes-in-the-box/whitney?utm_source=github.com&utm_medium=referral&utm_content=sonyccd/whitney&utm_campaign=badger)
 [![Build Status](https://travis-ci.org/sonyccd/whitney.svg?branch=master)](https://travis-ci.org/sonyccd/whitney)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/15de6cdaace9408e982ad2db14930c1a)](https://www.codacy.com/app/snakes-in-the-box/whitney?utm_source=github.com&utm_medium=referral&utm_content=sonyccd/whitney&utm_campaign=badger)
 
 A cotton counting system
--- whitney/find_cotton.py
+++ whitney/find_cotton.py
@@ -22,52 +22,54 @@ def test_thresh(img):
     fig, ax = filters.try_all_threshold(equalize)
 
 
-image = io.imread("cotton1.png")
+if __name__ == '__main__':
 
-image_gray = color.rgb2gray(image)
-image_gray = exposure.equalize_adapthist(image_gray)
-out = filters.rank.tophat(image_gray, morphology.disk(10))
-close = morphology.closing(out)
-opening = morphology.opening(close)
-bilat_img = filters.rank.mean_bilateral(opening, morphology.disk(30), s0=10, s1=10)
+    image = io.imread("../img/cotton1.png")
 
-blobs_doh = blob_doh(bilat_img, min_sigma=20, max_sigma=50, num_sigma=20, threshold=.006)
+    image_gray = color.rgb2gray(image)
+    image_gray = exposure.equalize_adapthist(image_gray)
+    out = filters.rank.tophat(image_gray, morphology.disk(10))
+    close = morphology.closing(out)
+    opening = morphology.opening(close)
+    bilat_img = filters.rank.mean_bilateral(opening, morphology.disk(30), s0=10, s1=10)
 
-fig, ax = plt.subplots(1, 1, sharex=True, sharey=True,
-                       subplot_kw={'adjustable': 'box-forced'})
+    blobs_doh = blob_doh(bilat_img, min_sigma=20, max_sigma=50, num_sigma=20, threshold=.006)
 
-ax.imshow(image, interpolation='nearest', cmap='gray')
-for blob in blobs_doh:
-    y, x, r = blob
-    c = plt.Circle((x, y), r, color='red', linewidth=2, fill=False)
-    rr, cc = draw.circle(y, x, r, shape=image.shape)
+    fig, ax = plt.subplots(1, 1, sharex=True, sharey=True,
+                           subplot_kw={'adjustable': 'box-forced'})
 
-    ax.add_patch(c)
+    ax.imshow(image, interpolation='nearest', cmap='gray')
+    for blob in blobs_doh:
+        y, x, r = blob
+        c = plt.Circle((x, y), r, color='red', linewidth=2, fill=False)
+        rr, cc = draw.circle(y, x, r, shape=image.shape)
 
-ax.text(1, 1, "Bolls: "+str(len(blobs_doh)), color='white',
-        bbox={'facecolor': 'black', 'alpha': 1, 'pad': 2})
-ax.set_axis_off()
+        ax.add_patch(c)
 
-plt.tight_layout()
-plt.show()
+    ax.text(1, 1, "Bolls: "+str(len(blobs_doh)), color='white',
+            bbox={'facecolor': 'black', 'alpha': 1, 'pad': 2})
+    ax.set_axis_off()
 
-# r = image[..., 0]
-# g = image[..., 1]
-# b = image[..., 2]
-# gauss = filters.gaussian(image_gray, sigma=0.5)
-# equalize = exposure.equalize_adapthist(gauss)
-# otsu = filters.threshold_otsu(equalize)
-# thresh = gauss <= otsu
-# thresh = 1 - thresh
-# close = morphology.binary_closing(thresh)
-# opening = morphology.binary_opening(close)
-# plt.imshow(opening, cmap='gray')
+    plt.tight_layout()
+    plt.show()
 
-# plt.figure(1)
-# plt.imshow(test_process(r))
-# plt.figure(2)
-# plt.imshow(test_process(g))
-# plt.figure(3)
-# plt.imshow(test_process(b))
+    # r = image[..., 0]
+    # g = image[..., 1]
+    # b = image[..., 2]
+    # gauss = filters.gaussian(image_gray, sigma=0.5)
+    # equalize = exposure.equalize_adapthist(gauss)
+    # otsu = filters.threshold_otsu(equalize)
+    # thresh = gauss <= otsu
+    # thresh = 1 - thresh
+    # close = morphology.binary_closing(thresh)
+    # opening = morphology.binary_opening(close)
+    # plt.imshow(opening, cmap='gray')
 
-# plt.show()
+    # plt.figure(1)
+    # plt.imshow(test_process(r))
+    # plt.figure(2)
+    # plt.imshow(test_process(g))
+    # plt.figure(3)
+    # plt.imshow(test_process(b))
+
+    # plt.show()
